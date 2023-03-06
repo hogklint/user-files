f03104d3e4b690c3874dbdab84e4403cc2efe269
Jimmie Högklint <hogklint@gmail.com>
2023-03-06 13:00:51 +0100

    Last update to setmonitor.sh
    Using autorandr instead but saving the final state at least

--- bin/setmonitor.sh
+++ bin/setmonitor.sh
@@ -5,8 +5,9 @@ HDMI1="HDMI-1"
 DP1="DP-2-3"
 DP2="DP-2-1"
 # Dock in left thunderbolt
-DP1="DP-3-3"
-DP2="DP-3-1"
+DP3="DP-3-3"
+DP1="DP-3-1"
+DP2="DP-3-2"
 
 CONNECTED_MONITORS="$(xrandr | grep -E "\sconnected" | cut -d' ' -f1)"
 
@@ -15,18 +16,18 @@ function is_monitor()
     echo "$CONNECTED_MONITORS" | grep -w $1
 }
 
-if [ -n "$(is_monitor $DP1)" ] && [ -n "$(is_monitor $DP2)" ]
+if [ -n "$(is_monitor $DP3)" ] && [ -n "$(is_monitor $DP2)" ]
 then
-    xrandr --output $DP1 --auto --primary --output $DP2 --auto --right-of $DP1 --rotate left --output $INTERNAL_MONITOR --off
-    echo "Dual: $DP1, $DP2"
+    xrandr --output $DP3 --auto --primary --output $DP2 --auto --right-of $DP3 --rotate right --output $INTERNAL_MONITOR --off
+    echo "Dual: $DP3, $DP2"
 elif [ -n "$(is_monitor $DP2)" ] && [ -n "$(is_monitor $HDMI1)" ]
 then
     xrandr --output $DP2 --auto --primary --output $HDMI1 --auto --right-of $DP2 --rotate left --output $INTERNAL_MONITOR --off
     echo "Dual: $HDMI1, $DP2"
-elif [ -n "$(is_monitor $DP1)" ] && [ -n "$(is_monitor $INTERNAL_MONITOR)" ]
+elif [ -n "$(is_monitor $DP3)" ] && [ -n "$(is_monitor $INTERNAL_MONITOR)" ]
 then
-    xrandr --output $DP1 --auto --output $INTERNAL_MONITOR --auto --left-of $DP1
-    echo "Dual: $INTERNAL_MONITOR, $DP1"
+    xrandr --output $DP3 --auto --output $INTERNAL_MONITOR --auto --left-of $DP3
+    echo "Dual: $INTERNAL_MONITOR, $DP3"
 elif [ -n "$(is_monitor $DP2)" ] && [ -n "$(is_monitor $INTERNAL_MONITOR)" ]
 then
     xrandr --output $DP2 --auto --output $INTERNAL_MONITOR --off
@@ -47,19 +48,23 @@ then
     #  --output eDP-1 --mode 3200x1800 \
     #  --output DP-1-2 --scale 2x2 --pos 3200x0 --panning 3840x2160+3200+0
     echo "Dual: $INTERNAL_MONITOR, $HDMI1"
-elif [ -n "$(is_monitor $DP1)" ]
+elif [ -n "$(is_monitor $DP3)" ]
 then
-    xrandr --output $DP1 --auto
-    echo "Single: $DP1"
+    xrandr --output $DP3 --auto
+    echo "Single: $DP3"
+elif [ -n "$(is_monitor $DP2)" ]
+then
+    xrandr --output $DP2 --auto
+    echo "Single: $DP2"
 elif [ -n "$(is_monitor $HDMI1)" ]
 then
     xrandr --output $HDMI1 --auto
     echo "Single: $HDMI1"
 elif [ -n "$(is_monitor $INTERNAL_MONITOR)" ]
 then
-    xrandr --output $INTERNAL_MONITOR --auto --output $DP1 --off --output $DP2 --output $HDMI1 --off
+    xrandr --output $INTERNAL_MONITOR --auto --output $DP3 --off --output $DP1 --output $HDMI1 --off
     sleep 2
-    xrandr --output $DP1 --off --output $DP2 --off --output $HDMI1 --off
+    xrandr --output $DP3 --off --output $DP1 --off --output $HDMI1 --off
     echo "Single: $INTERNAL_MONITOR"
 fi
 
